c0cf35db147bec4211d9377c113be7d69d005066
Samuel <kumasamuel94@gmail.com>
2024-02-10 09:42:05 +0100

    Display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,21 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 80 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge
+      fontSize={"14px"}
+      paddingX={1}
+      borderRadius="5px"
+      colorScheme={color}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
