3d133a0fd4e206e570f6ba752cdaf9052dc1165c
Samuel <kumasamuel94@gmail.com>
2024-02-10 10:45:49 +0100

    Show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card>
+      <Skeleton
+        height="200px"
+        width="200px"
+        borderRadius={10}
+        overflow={"hidden"}
+      />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
